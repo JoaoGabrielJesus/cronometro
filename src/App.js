cb63b4fb127163fcb3b67840cc8177b3567387c0
Joao Jesus <eng.joaojeus@gmail.com>
2021-07-22 12:26:01 -0300

    created design and cronometro img

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React, {Component} from 'react';
+import './style.css'
+
+class App extends Component{
+
+  constructor(props){
+    super(props);
+    this.state = {}
+  }
+
+  render(){
+    return(
+      <div className="container">
+        <img src={require('./assets/cronometro.png')} className="img" />
+        <a className="timer">0.0</a>
+        <div className="areaBtn">
+          <a className="botao">VAI</a>
+          <a className="botao">LIMPAR</a>
+        </div>
+      </div>
+
+    );
+  }
+}
+
+export default App;
